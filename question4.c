453bff61c3c29e5849c813da66a01b4e5f780a8d
Aditi8888 <88945828+Aditi8888@users.noreply.github.com>
2025-06-07 15:38:38 +0530

    Create question4.c

---
+++ question4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,even,odd;
+	printf("enter a number:\n");
+	scanf("%d",&a);
+	if (a%2==0)
+	printf("it is a even number\n");
+	else
+	printf("it is a odd number\n");
+	return 0;
+}
